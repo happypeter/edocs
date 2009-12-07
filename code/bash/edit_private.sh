760ee9fa1d6fb8f8d0f448f9e2b1c6cb689d1c22
pet <pet@girl.(none)>
2009-12-07 18:00:26 +0800

    nice script for private.gpg

--- code/bash/edit_private.sh
+++ code/bash/edit_private.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 ########I think I should always be careful about cwd
 cd /home/pet
+
+if [ -d abcd ]
+then
+echo "delete old abcd/"
+rm -rf abcd
+fi
 mkdir abcd
 cp edocs/private.tar.gz.gpg abcd/
 cd abcd/
 gpg -d private.tar.gz.gpg>private.tar.gz
 tar zxvf private.tar.gz
+rm *tar*
 cd private
 
 
