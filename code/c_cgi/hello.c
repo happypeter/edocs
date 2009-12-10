4d3a7deea1589d42cde36242110b5e6f919fb6ae
pet <pet@girl.(none)>
2009-12-10 09:57:43 +0800

    cgi in c

---
+++ code/c_cgi/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+
+{
+printf("Content-type: text/html\n\n");
+//the above line is a must, and double "\n" is a must 
+printf("hello");
+
+}
--- publish.sh
+++ publish.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 sudo rm /var/www/*.html
-sudo rm /var/www/css
-sudo rm /var/www/code
-sudo rm /var/www/pics
-sudo cp -rf css /var/www/
+sudo rm -rf /var/www/code
+sudo rm -rf /var/www/pics
 sudo cp -rf code /var/www/
 sudo cp -rf pics /var/www/
 
