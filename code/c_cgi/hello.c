6a13156b1935602a9555f2ad94fffea118151bca
pet <pet@girl.(none)>
2009-12-10 10:25:05 +0800

    edit code/ in emacs, great!!!

--- code/c_cgi/hello.c
+++ code/c_cgi/hello.c
@@ -4,7 +4,8 @@ int main()
 
 {
 printf("Content-type: text/html\n\n");
-//the above line is a must, and double "\n" is a must 
+//the above line is a must, and double "\n" is a must
+//I think this simple means a blank line is needed here 
 printf("hello");
 
 }
