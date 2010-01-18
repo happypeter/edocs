6f41532c335e3a48e16efa5f4d17ed3c87541a13
Peter King <happypeter1983@gmail.com>
2010-01-18 16:56:12 +0800

    defaut-msg

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ sudo rm -rf /var/www/code
 sudo rm -rf /var/www/pics
 sudo cp -rf code /var/www/
 sudo cp -rf pics /var/www/
-
+sudo rm -rf /var/www/*.html
 # need that emacs batch command as well
 #maybe I should integrate this one and edocs-commit
