a88e1d12cbb7f672653365ac085efee4c3d62e60
Peter Wang <happypeter1983@gmail.com>
2010-01-25 19:21:34 +0800

    change edocs/ structure to be similar to other muse projects

--- publish.sh
+++ publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo rm -rf /var/www/code
 sudo rm -rf /var/www/pics
-sudo cp -rf code /var/www/
+sudo cp -rf css /var/www/
 sudo cp -rf pics /var/www/
 sudo rm -rf /var/www/*.html
 # need that emacs batch command as well
