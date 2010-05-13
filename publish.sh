628fd02e020dfceef1f88cec7a4f62fffebae3b3
Peter Wang <happypeter1983@gmail.com>
2010-05-13 22:50:00 +0800

    publish.sh

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf /var/www/pics
-sudo cp -rf css /var/www/
-sudo cp -rf pics /var/www/
-sudo rm -rf /var/www/*.html
-# need that emacs batch command as well
-#maybe I should integrate this one and edocs-commit
-#new system is in use
