f537d5570a8b9a0ea1b9a7fb858688906f6331b2
Peter Wang <happypeter1983@gmail.com>
2010-05-16 22:22:48 +0800

    markdown

--- plaintext/muse2markdown.sh
+++ plaintext/muse2markdown.sh
@@ -2,4 +2,10 @@
 
 ## try to make a convertor here
 
+############ one solution #############
+# html2markdown convertor is available, so I can convert all muse file to html
+# and then html->markdown, then my work is done
+#######################################
+
 ## <example> to a newline and indent lines go between
+
