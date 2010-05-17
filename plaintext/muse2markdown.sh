e3940b5b60d6d4403b11fc36fa664c4ba9cafe57
Peter Wang <happypeter1983@gmail.com>
2010-05-17 09:25:04 +0800

    muse2markdown

--- plaintext/muse2markdown.sh
+++ plaintext/muse2markdown.sh
@@ -3,8 +3,10 @@
 ## try to make a convertor here
 
 ############ one solution #############
-# html2markdown convertor is available, so I can convert all muse file to html
+# html2markdown convertor is available,http://www.aaronsw.com/2002/html2text/
+# so I can convert all muse file to html
 # and then html->markdown, then my work is done
+# but It is better to change the html2text.py->muse2text.py
 #######################################
 
 ## <example> to a newline and indent lines go between
