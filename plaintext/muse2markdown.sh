a803fdcac0b82479253b1e05333d8eb18f739ab4
Peter Wang <happypeter1983@gmail.com>
2010-05-19 23:15:53 +0800

    header done

--- plaintext/muse2markdown.sh
+++ plaintext/muse2markdown.sh
@@ -9,11 +9,22 @@
 # but It is better to change the html2text.py->muse2text.py
 #######################################
 
+##########################################################################
 ## <example> to a newline and indent lines go between
 ################
 #cat new |sed 's/^<example>/\n/g'|sed 's/^<\/example>/\n/g'
 #
 #the above substitude <example> and </example> with 2 newlines
 ################
-cat $1 |sed '/<example>/,/<\/example>/  s/^/    / '|sed 's/<example>/\n/g'|sed 's/<\/example>/\n/g' > $1".mkd"
+cat $1 |sed '/<example>/,/<\/example>/  s/^/    / '|sed 's/<example>/\n/g'|sed 's/<\/example>/\n/g' > $1".1"
 ## so the above handle the <example> block well now
+###########################################################################
+
+
+
+############################################################################
+# "** header" -> "## header"
+
+cat $1".1" |sed 's/^\*\* /## /' > $1".2"
+
+# "^\*\*" means "**" leading a line
