b36682ac4a7a15d4361bae496e7604b3e216b17b
Peter Wang <happypeter1983@gmail.com>
2022-02-24 11:48:44 +0800

    casters

---
+++ sanding.c
@@ -0,0 +1,3 @@
+/*
+- layered cabinet to store sand papers https://youtu.be/_JeBHyRscRQ?t=479
+*/
--- workshop_plan.c
+++ workshop_plan.c
@@ -2,4 +2,5 @@
 - a place for track saw, drill press
 - bench has to stay in the center
   - power outlet from ceiling?
+- everything on casters https://youtu.be/_JeBHyRscRQ?t=309
 */
