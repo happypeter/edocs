73df9e7812b51eb46207dcb820cf59fa2c133b29
Peter Wang <happypeter1983@gmail.com>
2022-02-28 23:38:03 +0800

    led

---
+++ lighting.c
@@ -0,0 +1,128 @@
+/*
+## lighting_system
+- solution
+  - install track lighting, 50cm from the wall
+  - install pendant lights on the track, if appropriate
+    - https://www.bilibili.com/video/BV1W7411G7Z1?t=267.9
+  - pendant lights for each table, think crafts table
+- goal: hide light bulk, no light come directly to my eyes, the light bulb should targeting the things I want to see, not my eyes. 
+- pendant shade
+  - least curve, with simple pure shapes only, cubic, ball, half ball, cylinder etc. 
+    - https://item.jd.com/55261210582.html
+- even lighting is better lighting
+  - Use a broad light source. The broader the light source, the softer the light coming out of it. I am so happy about my MI lamp Pro.
+- led bulbs: main work horse, https://item.jd.com/7466255.html Opple warm(2700-3500k) light.
+- tips
+  - use Self Locking Electrical Cable Connector https://www.aliexpress.com/item/32961577684.html
+  - dark backgroud plus bright item https://youtu.be/p9286KaJjb0?t=181
+  - https://www.bilibili.com/video/BV1kt4y1Y7z3?from=search&seid=13948915908318026828
+- three types of lighting
+  - ambient: general alumination. Even though task and accent light also contribute to ambient lighting, but the ambient light set the predominant level and the mood of the space.
+  - task: reading
+  - accent: decorative lighting
+- my solution:
+  - use 3000k for all rooms as ambient, do keep all rooms the same to have a unified feel
+    - 'you don't want to exceed 3200k in your room' https://youtu.be/_ngjebdFVUY?t=374
+    - why warm light are relaxing? because it's the color of sun set. and we human realize it's end of the day, and time to relax. https://www.youtube.com/watch?v=LgM1jjSc0CE
+    - cool boy even suggest don't use anything higher than 4000k https://www.bilibili.com/video/BV1pD4y1R7RQ?t=10m31s not good for sleeping
+  - lady 小姜 use 2700k as ambient, and other place nothing over 3000k https://www.bilibili.com/video/BV1W7411G7Z1?t=539
+  - for living room, we may want 'enhancing light' (>4000k and high lumen) when I have parties, https://www.bilibili.com/video/BV1H4411q7os?t=4m3s the idea is if I want a >4000k and Full brightness, you can get it, but usually I will keep 3000k for all rooms
+  - >4000K for task light and accent light, these can vary from room to room
+  - use more than one pendant lamps for dinning table
+  - use some projection light for kitchen sink or stove, as task light
+    - https://www.bilibili.com/video/BV1pD4y1R7RQ?t=12m23s projection light bulk brand Tross 乔斯
+- ref: https://youtu.be/xeaeWoYH4z8?t=105
+  - a super small shade pendant light https://youtu.be/xeaeWoYH4z8?t=110
+- use shade for all bulb
+  - 'never never use light without screen' https://youtu.be/LgM1jjSc0CE?t=452
+- use just enough ambient light, dark background, make important things popup, e.g
+  - https://youtu.be/ENc-0dufe_c?t=139
+  - https://youtu.be/ENc-0dufe_c?t=226
+  - you can use projection light as ambient, target the wall, the light reflected by the wall is soft
+- temprature: ambient 3000k, task 4000k
+- ref:  https://www.bilibili.com/video/BV1W7411G7Z1?t=328
+- a pendant light for dinner table
+- floor lamp
+  - https://youtu.be/r4g-nOjU4NA?t=1034
+- bed room ceiling center lamp
+  - follow the cool guy here  https://www.bilibili.com/video/BV1pD4y1R7RQ/?
+    - have a soft center lamp to provide ambient light, no need to design task lights in advance, add them later, add as many as you want
+  - you need to spend some money on the projection light
+  - it only takes two screw to install https://www.bilibili.com/video/BV1pD4y1R7RQ/?t=108
+  - he said projection light is clear and beautiful
+    - 30-45 degree angle
+- ketchen lighting
+  - https://www.bilibili.com/video/BV1FA41147sS?t=5m
+  - pitfall: don't use projection light as task light, dark shade, at least not use one. preferably have more than one light for dish washing and dinner table.
+- and https://www.bilibili.com/video/BV1pD4y1R7RQ?t=83
+- creat layers https://youtu.be/ENc-0dufe_c?t=304
+- I want a KFC favor warm light for my room 'Ambient lighting'
+  - current solution is a 3w warm light led, I wonder how many lumens is needed for one squaremeter
+    - https://www.alconlighting.com/blog/residential-led-lighting/how-do-i-determine-how-many-led-lumens-i-need-for-a-space/
+      - 6w LED provides 450 lumens
+      - 9squaremeter bedroom needs 1000-2000lumens  
+- at least have three lights in one room
+  - https://www.bilibili.com/video/BV1pD4y1R7RQ?t=83
+  - my xiaomi table lamp serves my my 'task lighting'
+  - the idea is ambiient lighting should be 1/3 of the level of task lighting
+  - use one project light, targeting something beautiful.
+- it is not wise to have dark floor and bright ceiling
+  - it is wasteful
+  - so make the ceiling lamp lower with long cord
+- lumens
+  - 60w old style light bulbs only have 550 lumens
+  - best led can reach 6000lumen
+  - 15m2 room 12w/960lm light as Ambient
+    - the Chinese coolboy https://www.bilibili.com/video/BV1pD4y1R7RQ?t=83
+      - lots of good content here
+    - to my surprice, the cool boy use projection light as ambient light
+    - usually led 0.8w/m2 is good, or 80lm/m2 
+    - add as many task light as you want
+  - don't worry about wasting energy
+    - a very bright free stand lamp takes 41 days and nights to use 1kwh https://www.bilibili.com/video/BV1pD4y1R7RQ?t=83
+      - that is 0.1rmb per 24hr
+  - light temperature
+    - use same temperature for all rooms
+    - do not any light with temperature higher than 4000k
+      - it is not good looking to have a >4000 and <3000 bulk next to each other, task light can be whiter, not make it no higher than 4000
+    - CRI > 90
+  - https://www.bilibili.com/video/BV1W7411G7Z1?t=112
+    - kitchen lighting
+- three tips for interior lighting
+  - NO.1 Use multiple light sources
+  - NO.2 use dimmer
+    - bright for parties, soft for watching TV
+  - NO.3 Use right bulk
+    - led is good, save 90% of energy
+- calculate how much lumens you need
+  - https://youtu.be/LgM1jjSc0CE?t=452 
+    - for pathway 20lumens/squareFoot, 200lumens/squareMeter
+    - for dinning table 30lumens/squareFoot
+    - for task light 50lumens/squareFoot
+- led strips
+  - get 90degree https://youtu.be/Fu4qZW0pQm4?t=240
+  - don't mount led to wood, tooo hot for them https://youtu.be/Fu4qZW0pQm4?t=505
+    - LED strip channels too expensive, ~8RMB/m
+    - what I need is shadowless lights
+      - https://www.youtube.com/watch?v=sxPV8lIDLrc
+      - https://www.youtube.com/watch?v=CglpXFng_DE
+      - https://www.youtube.com/watch?v=cUyKYEyjcyc
+      - https://youtu.be/pwJxYi3q1Hs?t=677
+  - orders:
+    - rectifier 8RMB
+    - PDD: 11w/m, width: 17mm,  neural light, 180bulb/m 8.9RMB/m x 6m
+- recessed lights
+  - https://youtu.be/t3F0dwsBFjw?t=238
+  - orders
+    - JD: philips 2.5inch 3.5w 8RMBx5 
+      - 75mm hole drill, 7RMB
+  - recessed lights works well with led strips
+    - https://youtu.be/ENc-0dufe_c?t=92
+  - no track light or pendant for my workshop
+    - I may hit them with long wood
+    - same principles here, even accent lights will be strip or recessed 
+      - for my workshop
+      - I need both the wall-hung cabinet and lights here for my workshop
+        - https://youtu.be/ENc-0dufe_c?t=529
+      
+*/
