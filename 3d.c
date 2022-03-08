7f210abc295ced7fe6dbcce1fd5a53b1f61e9c50
Peter Wang <happypeter1983@gmail.com>
2022-03-08 21:33:54 +0800

    lame GUI

---
+++ 3d.c
@@ -0,0 +1,37 @@
+/*
+  - plainText first
+    - I don't like GUI things, 3d software is just another one of these
+    - I love markdown, I love css
+      - since I can render html if I need
+        - As to my woodwork, I want to just keep every detail in code
+        - When I need to render my project, I render it with wood to real world
+          - So if in the future, I want to upgrade my design
+          - I can take a peek at the photo(or real object) of my desgin
+          - if 3d design file is for the sake of quickly get the idea of what you are going to build
+            - I think .jpg file will also do the job
+      - I can versionControl/Global them,
+    - engineering is all about iteratons
+      - A .skp file is not git friendly
+      - to build sth(woodwork or device development), there is a lot more than just shapes
+        - materials/workflow/watchouts/tests(think unitTest & IntegrationTest)/
+        - everything will be much more managable(esay to update/modularize)
+          - in a 0 redundant way
+          - sketchup also has Components
+            - but can you pass parameters to it
+            - once you make one Unique, can you change it along with its slibings
+              - since shapes are not easily searchable
+              - so use 3d files to manage a complicated project will not be doable
+              
+    - PlainText is good
+      - If I want a cutList, I can just use a array
+        - Why use a graph? GUI things is mainly for the bosses or the customer
+      
+  - https://www.youtube.com/watch?v=AVb8ZBAOD0I
+
+- https://www.youtube.com/watch?v=rjSE28T3u78
+  - April Wilkerson
+- OPEN THE COMPONENT BEFORE EDITING IT
+  - https://youtu.be/r2K6gmcc_jA?t=1712
+- tripple click to select a object that is hard to select cleanly with mouse https://youtu.be/vNf_YiBC_g0?t=574
+- rotate tool: copy/rotate/shorten https://youtu.be/vNf_YiBC_g0?t=741
+*/
