41691964203affd7d72899a05f334b0aad17d7da
Peter Wang <happypeter1983@gmail.com>
2022-03-18 07:49:46 +0800

    welding

---
+++ weld.c
@@ -0,0 +1,22 @@
+int choose_machine() {
+  /*
+  - should I buy one just for stickWelding or allInOne?
+    - https://www.youtube.com/watch?v=vUZkeYFOu1E
+    - you can buy machine than does both MIG and Stick 
+      - PDD: 不用气的二保焊机
+    - I will first rule out all methods with GasProtection
+      - hard to buy the gas
+      - but I do want a machine that works with wider range of thickness
+        - even though without Gas, the result will be uglier, even you use thin wire(the MIG-ish way)
+  - stick machine is cheap
+    - should work for 1.5mm or thicker
+  - tig 氩弧焊 needs a gas tank, less portable
+    - the skill is harder to learn https://youtu.be/OWThL97tq3k?t=134
+    - prettier result, and for thinner targets
+      - just melt the metals and fuse them together, no intermidiary metal needed
+    - MIG also needs a gas tank, but it use a feed wire
+      - so it's easier than TIG, and less quality
+    - it is a subtype of Gas metal arc welding 气保焊
+      - 二保焊 is also a subtype, the gas is CO2, hence the name
+  */
+}
