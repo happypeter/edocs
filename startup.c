cc6f07dd43871e4883da14df4a9bd41cd0c40e4e
Peter Wang <happypeter1983@gmail.com>
2022-04-05 06:41:28 +0800

    beyond farming

--- startup.c
+++ startup.c
@@ -11,4 +11,5 @@
     - I hated my former software business, cause it was more like customization sevice, you need to plz each customer in different way
     - I love optimize for a certain process, seeing productivity grows each year, my family blood is 1000 years of farmers.
     - My father said rich people was bad. Yes, I still think bussiness people are evil in a certain way.
+  - or I can make tables/ladders something new era DIYer's like me will love
 */
