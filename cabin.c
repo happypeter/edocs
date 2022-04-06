fbfc54d3b03283f99ae69116f8c018d3fb8247c9
Peter Wang <happypeter1983@gmail.com>
2022-04-06 08:38:39 +0800

    legal

---
+++ cabin.c
@@ -0,0 +1,37 @@
+
+
+/*
+- ref: https://www.youtube.com/watch?v=H1UTgWED7pI
+
+*/
+
+
+/*
+- called 秦皇岛市建设局 they say you call 3552757 海港区住建局
+  - which seems to me they don't give a dame if you build a portable house in your yard
+*/
+
+is_legal() {
+
+}
+
+/*
+- a slab?
+  - since it's not easy to dig deep into my bricky ground
+  - a slab seems easier
+- posts?
+  - https://youtu.be/42B-Db1V1C8?t=382
+  - with treated wood https://youtu.be/6eHGFoMTAlw?t=119
+  - with brackets https://youtu.be/RcoBG6phr58?t=880
+    - install brackets when posts are done https://youtu.be/61-X2-c4UR0?t=805
+- wood frame floor https://youtu.be/bOOXmfkXpkM?t=502
+*/
+int foundation() {
+
+}
+
+/*
+- with C and U studs https://www.youtube.com/watch?v=c6XVqx7DT-s
+*/
+
+int framing()
