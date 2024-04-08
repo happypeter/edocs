6aede0a8c7e4dc1bbdd142403f0e7a098a2272a2
Peter Wang <happypeter@users.noreply.github.com>
2024-04-09 00:13:37 +0800

    Update shoe.js

--- shoe.js
+++ shoe.js
@@ -1,1 +1,8 @@
-【看看日本主妇的收纳有多厉害！roomtour  | 123㎡日本关西40年房龄住宅。-哔哩哔哩】 https://b23.tv/K6iw2my
+const shoe_storage = {
+
+  /*
+  this video convinced me doors r essential to any home: 【看看日本主妇的收纳有多厉害！roomtour  | 123㎡日本关西40年房龄住宅。-哔哩哔哩】 https://b23.tv/K6iw2my
+  */
+}
+
+
