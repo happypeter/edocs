dcdee213c4e88ed7f0653158ded8aac40a5e4254
Peter Wang <happypeter1983@gmail.com>
2024-11-29 09:33:24 +0800

    stove top table

--- stove_top_table.js
+++ stove_top_table.js
@@ -2,9 +2,19 @@
  * plant stove to 
  */
 
+
+const feathers = {
+  /**
+   * - small overhang, so that wok won't be covered
+   *   - 3cm over hang, and if the side pannel installed, the over hang will be 1.5cm or so
+   * - the space to the right of thhe coal stove will be home for a kitchen cart
+   *   - so make sure no bottom strechers will stay in the way
+   * - front/back aprons will be lap joineries, side strechers will be pocket dowels, not screws, dowels
+   */
+}
 const space = {
-  plant_to_coal: 7.5, // cm from plant stove to coal one
-  plant_to_left_edge_coal: 44, // 108-44 is the width I have to build shelves,
+  wok_to_coal: 7.5, // cm from wok stove to coal one
+  wok_to_left_edge_coal: 44, // 108-44 is the width I have to build shelves,
 }
 
 const sizes = {
