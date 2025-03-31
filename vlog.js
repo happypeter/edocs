2ff34e2f0da44f5fc3c860ab6a12c853bae908e7
Peter Wang <happypeter1983@gmail.com>
2025-03-31 21:39:10 +0800

    cool

--- vlog.js
+++ vlog.js
@@ -1,49 +1,10 @@
-
-/**
- * 
- */
-
-
-export const vlog = () => { 
-   opening()
-}
-
-const trasaction_frame = () => { 
+const process = {
   /**
-   * a black white theme will do https://youtu.be/vMYUzhbl02w?t=8 */ 
-}
-
-const text = () => { 
-  /**
-   * like https://youtu.be/Hv5UvqcsIfY?t=106
+   * - steps:
+   *   - shot with iPhone
+   *   - airdrop all footable for today to iPad
+   *   - Capcut to rough select the parts I need
+   *   - export to MOV file in 1080p, add Title and date in the video
+   *   - no extra editing, and save the mov on my Mac Mini at project_name/timestamp.mov, e.g shed/20250331.mov
    */
-  transparent_bg() 
-}
-const static_sence = () => { 
-  /**
-   * don't move the camera when you are shotting one seen
-   * unless it is clearly necessary */ 
-}
-
-const insert_fonts = () => { 
-  /**
-   * with screenflow
-   * png with https://www.diyiziti.com/meishu
-   *  */ 
-}
-
-const opening = () => { 
-  /**
-   * start with some music 
-   * https://www.youtube.com/watch?v=Hv5UvqcsIfY
-   *  */ 
-}
-
-const slow_motion = () => { 
-   
-}
-
-const insert_gif = () => { 
-  /**
-   * like moving text transations https://youtu.be/Hv5UvqcsIfY?t=197 */ 
 }
