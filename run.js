f4b9e2a1663f4b2b1498c1bbeee4ddcadac18cbb
Peter Wang <happypeter1983@gmail.com>
2025-09-02 11:46:23 +0800

    harbin

--- run.js
+++ run.js
@@ -1,7 +1,96 @@
 pace_the_marathon = {
-  /** I did a 42k training today, and I learnt some
+  heat_rate_based: {
+     /** I did a 42k training today, and I learnt some
    * - start from 100, use the first 3k to go to 140, no rush
    * - stay below 149bpm, never go higher, otherwise lactic acid slow you down significant, bad deal
    * - the last 10k go 160bpm
+   */ 
+  },
+  speed_based: {
+    /**
+     * more pro approach is to pace with min/km, make it possible to run faster later half
+     * then I should pace at 5:50
+     */
+    slower_first_half: {
+      ressons: {
+        /**
+         * bank your energy, not your time, cause 1min you gain from running too fast
+         * in the early stage, 2min you will give back
+         */
+        lactic_buildup: {
+        /**\
+         * OK, you started too fast, and your lactic acid added up too fast,
+         * and you end up going with pain in you calves
+         */
+        },
+        glycogen_burn_faster: {
+          /**
+           * the faster you go, the less you will use your fast
+           * so you endup using up your glycogen storage in the first half
+           * while you actually need it more in later half
+           */
+        }
+      },
+
+      phyological_factor: {
+        /**
+         * having you pass others in later race is sweet
+         */
+      }
+    },
+    follow_the_pacer: {
+      /**
+       * I just need to follow the 400 pacer
+       */
+      benefits: 'saving brain energy to check watch constantly'
+    }
+  }
+
+}
+
+fuel = {
+  /** 
+   * never try anything unfamiliar, they do cause issues
+   * 
+   */
+  carb_load: {
+    /**
+     * just rice, try no protein or fat or vegie at all
+     */
+  }
+}
+
+harbin_2025 = {
+  /**
+ Harbin Marathon 2025 Lessons
+
+ #how do you feel does not matter
+ 
+ Before I really started to run, I felt really bad, low energy, stomach pain. but when the reced ends, I joyfully had 2 ice drinks and a ton of ice food without any issue.
+ the reason was I had a full day of real rest(despite a bed night of sleep), real amount energy was prepared.
+ and if there is real store of energy there, bad feeling at the beginning of the race does not matter.
+ I slowed down a bit 5k after I started to run. got my heartrate lower to 140 or so. I felt prefect again after 2k of that.
+ then I went at 160plus throughout the rest of the race.
+
+
+ # the fuel berfore and during
+
+For carbloading, I had rice with toppings in the morning, but somehow I had slight diaarhea after. probably due to the oil from the toppings.
+so for the afternoon, I had KFC, despite the protein and ice coke, I felt prefect. so I will use KFC for the furture.
+
+I went without any gel, I think it worked for me. I had 5 salt tablet. enough energy drinks.
+I went for 2 small pieces of banana, without any issue.
+
+# pace
+
+in the case of this race, I wasted around 40min waiting at the first 8k, so I went
+at 5:00 or so for the first a few km, then 5:40 or so for the rest. 
+
+Now the lesson should be keep a stead pace. I think 5:50min/k is just perfect for me.
+Yes, try to keep the same pace even at the kms with slopes and fueling stations.
+
+conclusion: I will do 5:50 for at least the first 35km.
+
+
    */
 }
