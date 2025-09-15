9a541dfac06c894b80d95aa55d6fa4635c7cb7db
Peter Wang <happypeter1983@gmail.com>
2025-09-15 19:26:06 +0800

    shenyang marathon

--- run.js
+++ run.js
@@ -72,6 +72,9 @@ harbin_2025 = {
  I slowed down a bit 5k after I started to run. got my heartrate lower to 140 or so. I felt prefect again after 2k of that.
  then I went at 160plus throughout the rest of the race.
 
+ # gears
+ the Flipbelt pants worked like a charm. NO.1 not more waist bags, the front pocket worked better than waist bag. NO.2 it still felt comforatable when wet
+ the cheap black tank top was OK, just a bit heavy when get wet, will buy a more expansive one later.
 
  # the fuel berfore and during
 
@@ -91,6 +94,67 @@ Yes, try to keep the same pace even at the kms with slopes and fueling stations.
 
 conclusion: I will do 5:50 for at least the first 35km.
 
+# Cooling down
 
+it was necessary to pour water on head to cool my body done, but getting my shoes wet was not good idea.
+
+   */
+}
+
+
+shenyang_2025 = {
+  /**
+   * there are so many factors that may make or break your race. I got a PB at 
+   * 3:56min. BUT I think there r still 2 min problems and a few usful tips I wanna share.
+   * 
+   * ## Pre-fuel and fuel
+   * 
+   * I make a siginiifcant mistake for my two days carbload: the first day, I had
+   * too much food, 5 bowl of rice at krean rice pot, and later too much chicken
+   * and bean product in the evening. so the seond day, that was the day before the race
+   * I did not feel hungry, so I did not eat enough. that why I had to have
+   * breakfast the race day morning, and stupidly, I have too much and even meat
+   * buns. that result in real pressure for the first half of the race, a bit pain
+   * in the stomach, and could not take in as much water as I wanted.
+   * 
+   * my fix would be, for the first carbload day, try less proteion, and the
+   * race day breakfast. no matter how early, eat very little, no oil or protein at all.
+   * 
+   * My mistake during the race was too things: I forgot to have any salt pills
+   * for the seond half, in such a dog day, that was stupid, that could be the 
+   * cause of the weird fatigue at my last 2k.
+   * 
+   * ## During
+   * 
+   * ## Negative Split
+   * I end up did a negative split, and I don't regret that. everybody knows, the 
+   * ideal case is that you pace steadily. but realilty is complicated, sunshine, wind,
+   * uphills. same effect may not result in same speed, giving too much effect on
+   * a uphill just try to be stead with your pace does not seem a good idea.
+   * 
+   * so my solution is, first base on your current ability, set your average pace. and
+   * then go slower for the first half. when encountering uphills, try not to send your
+   * heatrate too high, even that means you need to go slower. you know, that fact
+   * is that, for every 1min you go too fast through the first half, you will pay
+   * 3to5 times more back. 
+   * 
+   * In the case of my Shenyang race. For the first 10k, may heartrace is around 130. felt really
+   * easy. 10-20k, I tried to keep the same my 530 pace, but heart rate went to 150, but my bottom line was to keep the avarage below
+   * my lactate threshold. from 24k, it turns to 160 or so. my max, which is 174 happened right before I hit the finish. and my average was 153.
+   * lucklily my pace was mostly kept during the whole course.
+   * 
+   * I know even exprienced people will say negative split is too hard for ordinary people,
+   * I know I may also change in the future. But right now, keeping the first half
+   * consevative just a safe approach, without bunking in the second.
+   * 
+   * ## Tips
+   * tip number one: when going uphill, find a pretty and folow her, keep your eyes on her heels and never look the slope, that really helped.
+   * tip number two: taking off your cap, in shades. let the wind cool you down.
+   * 
+   * 
+   * ## Conclusion
+   * 
+   * It was fun.
+   * 
    */
 }
