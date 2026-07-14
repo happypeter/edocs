f1ac591fc6e7583f7cedf9753ef19f872a57aac2
Peter Wang <happypeter1983@gmail.com>
2026-07-14 22:31:26 +0800

    i

--- chicken.js
+++ chicken.js
@@ -7,3 +7,19 @@
  *     - July 10 2026: I moved the chickens to the run, they ate a lot more than they were in cage for the last 7 days.
  *       - as AI told, keep free range chicken, like Chinese Chaiji in cage can cause issues
  */
+
+const mars_coop = {
+  solution: 'https://youtu.be/yR6TD4tAoUk?t=664',
+  /**
+   * love the design, feeder, waterer and most of things
+   */
+  /**
+   * - features
+   *   - sth small and movable https://www.youtube.com/watch?v=rNY8iIV8zKk
+   *     - with a closed run that allow chickens to stay out at night for summers
+   *   - https://youtu.be/yR6TD4tAoUk?t=664
+   *     - use vinyl for floor
+   * - todo:
+   *   - a PVC feeder https://www.youtube.com/watch?v=yR6TD4tAoUk
+   */
+}
--- tomato.js
+++ tomato.js
@@ -2,8 +2,19 @@ const prune = {
   /**
    * keep only main stem and one sucker
    * - do topping when there is already enough fruit, and the season is ending
+   * - remove all low things
+   *   - sucker or leaves, to avoid deasese from soil
+   *   - and you get enhanced airflow
+   *   - ANYTHING touch ground https://www.youtube.com/shorts/IwuzTfWYbw8
+        * - one approach: REMOVE ALL suckers
+    - when
+      - when the sunshine is strong, to avoid infection
    */
-<<<<<<< HEAD
+  reason: {
+    /**
+     * remove leaves that receive no sunshine
+     */
+  }
 }
 
 const note = {
