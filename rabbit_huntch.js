e65cc8919da71490679ae29d895315e620f9288a
Peter Wang <happypeter1983@gmail.com>
2026-07-16 21:43:55 +0800

    hutch

--- rabbit_huntch.js
+++ rabbit_huntch.js
@@ -1,38 +1,12 @@
-/**
- * - goals
- *   - vlog_tip: show people the end result, and explain the goals
- *   - weather proof
- *   - a openable roof
- *   - wire inside, so that rabit wont chew on the wood https://youtu.be/6nN3vSnYPzM?t=897
- *   - all wire floor, no wood at all, easy to be kept clean
- *     - the support needs to be strong and paint, meal? so that sparyable
+/*
+ *   - wire floor, no wood at all, easy to be kept clean
  *   - a bit distance from ground, so that I can easily get the poop out https://youtu.be/6nN3vSnYPzM?t=1068
- *   - stick supportable roof https://youtu.be/6nN3vSnYPzM?t=184
+ *   - front doors rather than roof door
  *   - sheathed plywood wall room on west side to shade away hot setting sun
- *   - a hey station: https://youtu.be/6nN3vSnYPzM?t=647
- * 
- * - material collection
- *   - vlog_tip: tell the audience this is the step I do, but don't introduce things in detail
- *   - faced plywood
- * - design
- *   - a simple plan drawing for myself
- *   - mostly replic this 
- *     - the structure not strong shear force https://www.youtube.com/watch?v=EgaE1R5IJQI
-       - https://youtu.be/6nN3vSnYPzM?t=1067
- *       - use plywood cover half of the structure to provide shear strength 
- *   - course_tip: don't explain it too much to the audience, cause things will get clear along building
- * - process 
- * - Next Version aka. v2
- *   - plan, my porcreate App, RabbitHutchV2
-  * - wire cloth
-  *   - the one I got is 1mm thick with 2cm holes, I will prefer 1.5mm thick and 1.5cm holes for my next version of this
-  *   - for the floor, PVC coat wire mesh is prefered to avoid foot sores
+ *   - roof
+ *     - shade above roof, or make roof well insulated
   * - depth
   *   - is now 60cm, I will make it 70cm next version
-  * - roof
-  *   - I will use sheet metal roof, but add a big handle infront, and maybe some wood corners to hide the dark edges
-  *     - https://www.youtube.com/shorts/FQIKcTCORrM
-  *   - flatter roof, so that I don't need to cut angles for roof frame
   * - floor
   *   - thick wire flooring https://youtu.be/6nN3vSnYPzM?t=383
   *     - throw hardwareCloth+mat, embrace tougher wire bottom floor
@@ -43,8 +17,7 @@
 
 rabbits_info = {
       /**
-       * gray one is a lady, 比利时兔, most common of all in China
-       * white one is man, hybrid from 巴扎黑 and 白塔兔 
+        white one is man, hybrid from 巴扎黑 and 白塔兔 
        */
       heat: {
             /** seller said nothing needed to enhance the airflow in summer
@@ -53,16 +26,6 @@ rabbits_info = {
              */
       },
       food: {
-            furit_tree_leaves: {
-                  // apple, pear, but not chestnut
-                  // dandelion is good
-
-            },
-            twigs: {
-            // apple or pear
-                  // apricot twigs r not safe
-
-            },
             toxic: {
                   /**
                    * cabbage and spinech are also OK for rabbits
