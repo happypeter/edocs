b7fca0f7d0e900bdd5b8445d9ad9c29905bf8f50
Peter Wang <happypeter1983@gmail.com>
2026-08-24 22:22:17 +0800

    i

---
+++ meal.js
@@ -0,0 +1,51 @@
+principles = {
+  batch_prep: 'prefarbricate the food in one go for one week',
+  zero_cooking: 'then each meal cooking is nothing but heathing up and serve',
+  non_oily: {
+    /***
+     * meat with be cooked with less soup
+     * so when served, the chucks should be juicy not not drippy
+     * think a beef salad, like https://susiecooksthai.com/thai-beef-salad-nam-tok-recipe/
+     * 
+     */
+  },
+  small_storage_units: {
+    /**
+     * my freezer section size:28tallx32widex30deep
+     * 
+     * freezer door bins: 9.5x24(height)x28long
+     * 
+     * keeping my lego blocks small is a must, so the main container
+     * will mostly be the SuperCube ice trays. preferbly of different color and size
+     * - other storage containers
+     *   - bags: https://www.youtube.com/shorts/Rv11sfiDqp4
+     *     - frige as a file cabinet https://youtu.be/LdK2Wt16hyg?t=201
+     *     - press thin, stand them in a box, all bags can be seen
+     *       - e.g https://youtu.be/-RT3Ettd_6c?t=293
+     *   - small butter molds https://youtu.be/yg4d1n3yae4?t=105
+     * 
+     * 
+     */
+  }
+}
+
+tricks = {
+  ice_store: 'use silicone frige tray https://www.bilibili.com/video/BV18KLq6ZEL5/?t=1m13s',
+  steamer: {
+    /**
+     * https://www.bilibili.com/video/BV18KLq6ZEL5/?t=1m56s
+     */
+  }
+
+}
+
+inspirations = {
+/***
+ * https://www.bilibili.com/video/BV1hs421379U/
+ */
+  quick_pickel: {
+    /**
+     * https://youtu.be/xAIerfe3m8U?t=730
+     */
+  }
+}
